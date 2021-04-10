ce98f7adece837d0220d3fccb63cec038c0f8f47
Ilan Olkies <ilanolkies@outlook.com>
2021-04-09 21:12:50 -0300

    Add timestamp validation and other validatoins

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -2,27 +2,34 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Forwarder {
+  uint window;
+
   struct Metatransaction {
     address to;
     bytes data;
     uint gas;
+    uint timestamp;
     uint value;
     bool executed;
   }
 
-  Metatransaction[] private bag;
-
-  event MetatransactionAdded(uint indexed index, address to, bytes data, uint gas, uint value);
-  event MetatransactionExecuted(uint indexed index, bytes result);
-
-  function add(address to, bytes memory data, uint gas) public payable {
-    bag.push(Metatransaction(to, data, gas, msg.value, false));
-    emit MetatransactionAdded(bag.length - 1, to, data, gas, msg.value);
+  constructor(uint _window) {
+    window = _window;
   }
 
-  function at(uint index) public view returns(address, bytes memory, uint, uint, bool) {
+  Metatransaction[] private bag;
+
+  event MetatransactionAdded(uint indexed index, address to, bytes data, uint gas, uint timestamp, uint value);
+  event MetatransactionExecuted(uint indexed index, bool succes, bytes result);
+
+  function add(address to, bytes memory data, uint gas, uint timestamp) public payable {
+    bag.push(Metatransaction(to, data, gas, timestamp, msg.value, false));
+    emit MetatransactionAdded(bag.length - 1, to, data, gas, timestamp, msg.value);
+  }
+
+  function at(uint index) public view returns(address, bytes memory, uint, uint, uint, bool) {
     Metatransaction memory metatransaction = bag[index];
-    return (metatransaction.to, metatransaction.data, metatransaction.gas, metatransaction.value, metatransaction.executed);
+    return (metatransaction.to, metatransaction.data, metatransaction.gas, metatransaction.timestamp, metatransaction.value, metatransaction.executed);
   }
 
   function execute(uint index) public {
@@ -30,12 +37,22 @@ contract Forwarder {
 
     require(!metatransaction.executed, "Already executed");
 
+    // Here we can add refund business logic instead of prohibiting execution
+    require(metatransaction.timestamp - window < block.timestamp, "Too soon");
+    require(metatransaction.timestamp + window > block.timestamp, "Too late");
+
+    // We can use gasleft() here to charge the consumer for the gas
+    // A contract may hold user's gas and charge it after executing
+    // the transaction
+
+    // Now failing transactions are forwarded. Is responsability of the requestor
+    // to list a valid transaction
     (bool success, bytes memory result) = metatransaction.to.call{ gas: metatransaction.gas, value: metatransaction.value }(metatransaction.data);
 
-    require(success, string(result));
-
+    // The difference when calling gasleft() again is (aprox.) the gas used
+    // in the call
     metatransaction.executed = true;
 
-    emit MetatransactionExecuted(index, result);
+    emit MetatransactionExecuted(index, success, result);
   }
 }
--- test/forwarder.test.js
+++ test/forwarder.test.js
@@ -3,30 +3,38 @@ const Forwarder = artifacts.require('Forwarder')
 
 const assert = require('assert')
 
-const incData = web3.utils.sha3('inc()').slice(0, 10)
+const getMethodSig = method => web3.utils.sha3(method).slice(0, 10)
+const incData = getMethodSig('inc()')
+const failData = getMethodSig('fail()')
+const window = 1800
+
+const solidityError = message => ({
+  message: `Returned error: VM Exception while processing transaction: revert ${message} -- Reason given: ${message}.`
+})
 
 contract('Forwarder', () => {
   beforeEach(async () => {
     this.counter = await Counter.new()
-    this.forwarder = await Forwarder.new()
+    this.forwarder = await Forwarder.new(window)
   })
 
   describe('listing', () => {
     beforeEach(() => {
       this.testListingWithValue = async (value) => {
         const to = this.counter.address
-        const data = web3.utils.sha3('inc()').slice(0, 10)
         const gas = new web3.utils.BN(await this.counter.inc.estimateGas())
+        const timestamp = new web3.utils.BN(Math.ceil(+Date.now() / 1000))
 
-        await this.forwarder.add(to, data, gas, { value })
+        await this.forwarder.add(to, incData, gas, timestamp, { value })
 
         const actual = await this.forwarder.at(0)
 
         assert.strictEqual(actual[0], to)
-        assert.strictEqual(actual[1], data)
+        assert.strictEqual(actual[1], incData)
         assert.strictEqual(actual[2].toString(), gas.toString())
-        assert.strictEqual(actual[3].toString(), value.toString())
-        assert.strictEqual(actual[4], false)
+        assert.strictEqual(actual[3].toString(), timestamp.toString())
+        assert.strictEqual(actual[4].toString(), value.toString())
+        assert.strictEqual(actual[5], false)
       }
     })
 
@@ -36,16 +44,19 @@ contract('Forwarder', () => {
 
   describe('execution', () => {
     beforeEach(() => {
-      this.testExecutionWithValue = async (value) => {
+      this.addAndExecuteWithTimestamp = async (value, timestamp) => {
         const to = this.counter.address
-        const data = web3.utils.sha3('inc()').slice(0, 10)
         const gas = new web3.utils.BN(await this.counter.inc.estimateGas())
 
-        await this.forwarder.add(to, data, gas, { value })
+        await this.forwarder.add(to, incData, gas, timestamp, { value })
 
         await this.forwarder.execute(0)
+      }
 
-        assert.strictEqual(await this.forwarder.at(0).then(meta => meta[4]), true)
+      this.testExecutionWithValue = async (value) => {
+        await this.addAndExecuteWithTimestamp(value, new web3.utils.BN(Math.ceil(+Date.now() / 1000)))
+
+        assert.strictEqual(await this.forwarder.at(0).then(meta => meta[5]), true)
         assert.strictEqual(await this.counter.count().then(r => r.toString()), '1')
         assert.strictEqual(await web3.eth.getBalance(this.counter.address).then(r => r.toString()), value.toString())
       }
@@ -53,5 +64,52 @@ contract('Forwarder', () => {
 
     it('executes a listed a metatransaction', () => this.testExecutionWithValue(new web3.utils.BN(0)))
     it('executes a listed a metatransaction with value', () => this.testExecutionWithValue(new web3.utils.BN(1e15)))
+
+    it('cannot execute twice', async () => {
+      await this.testExecutionWithValue(new web3.utils.BN(0))
+      await assert.rejects(
+        this.forwarder.execute(0),
+        solidityError('Already executed')
+      )
+    })
+
+    it('cannot execute before timestamp - window', () => assert.rejects(
+        this.addAndExecuteWithTimestamp(0, new web3.utils.BN(Math.ceil(+Date.now() / 1000) + window + 3000)),
+        solidityError('Too soon')
+      )
+    )
+
+    it('cannot execute after timestamp + window', () => assert.rejects(
+        this.addAndExecuteWithTimestamp(0, new web3.utils.BN(Math.ceil(+Date.now() / 1000) - window - 3000)),
+        solidityError('Too late')
+      )
+    )
+
+    describe('failing metatransactions', () => {
+      it('due to revert in called contract', async () => {
+        const to = this.counter.address
+        const gas = new web3.utils.BN(await this.counter.inc.estimateGas())
+        const timestamp = new web3.utils.BN(Math.ceil(+Date.now() / 1000))
+
+        await this.forwarder.add(to, failData, gas, timestamp)
+
+        const tx = await this.forwarder.execute(0)
+        const log = tx.logs.find(l => l.event === 'MetatransactionExecuted')
+        assert.ok(!log.args.success)
+        assert.ok(Buffer.from(log.args.result.slice(2), 'hex').toString('utf-8').includes('Boom'))
+      })
+
+      it('due to insufficient gas in called contract', async () => {
+        const to = this.counter.address
+        const gas = new web3.utils.BN(10)
+        const timestamp = new web3.utils.BN(Math.ceil(+Date.now() / 1000))
+
+        await this.forwarder.add(to, failData, gas, timestamp)
+
+        const tx = await this.forwarder.execute(0)
+        const log = tx.logs.find(l => l.event === 'MetatransactionExecuted')
+        assert.ok(!log.args.success)
+      })
+    })
   })
 })
