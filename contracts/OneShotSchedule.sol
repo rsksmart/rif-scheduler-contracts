2a8e73f26f01950f35797bd6faa2e2aeecc50a53
Diego Grasso <diegograsso@192.168.1.5>
2021-04-14 17:21:06 -0300

    Accept payments

--- contracts/OneShotSchedule.sol
+++ contracts/OneShotSchedule.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@rsksmart/erc677/contracts/IERC677.sol";
+// import "@rsksmart/erc677/contracts/IERC677Receiver.sol";
 
 contract OneShotSchedule {
   uint window;
+  IERC677 token; 
+  uint price;
 
+  mapping(address => uint) remainingSchedulings;
+  
   struct Metatransaction {
+    address from;
     address to;
     bytes data;
     uint gas;
@@ -13,21 +20,43 @@ contract OneShotSchedule {
     bool executed;
   }
 
-  constructor(uint _window) {
-    window = _window;
-  }
-
   Metatransaction[] private bag;
 
   event MetatransactionAdded(uint indexed index, address to, bytes data, uint gas, uint timestamp, uint value);
   event MetatransactionExecuted(uint indexed index, bool succes, bytes result);
+
+  constructor(IERC677 _rifToken, address _providerAccount, uint _price, uint _window) public {
+    window = _window;
+    token = _rifToken;
+    price = _price;
+    window = _window;
+  }
+
+  function _totalPrice(uint _amount) private returns (uint){
+    return _amount * price;
+  }
+
+  function purchase(uint _amount) public {
+    //require(token.allowance(msg.sender, address(this)) >= _totalPrice(_amount), 'Allowance Excedeed');
+    token.transferFrom(msg.sender, address(this), _totalPrice(_amount));
+    remainingSchedulings[msg.sender] += _amount ;
+  }
+
+  function getRemainingSchedulings(address _requestor) public view returns(uint){
+    return remainingSchedulings[_requestor];
+  }
+
+  function spend(address _requestor) private {
+    require(remainingSchedulings[_requestor] > 0);
+    remainingSchedulings[_requestor] = remainingSchedulings[msg.sender] - 1;
+  }
 
   function schedule(address to, bytes memory data, uint gas, uint timestamp) public payable {
     // We should charge the user for the execution. Refund is given if service provider
     // fails to execute the transaction
 
     // Important! Check the schedule is not for the past
-    bag.push(Metatransaction(to, data, gas, timestamp, msg.value, false));
+    bag.push(Metatransaction(msg.sender,to, data, gas, timestamp, msg.value, false));
     emit MetatransactionAdded(bag.length - 1, to, data, gas, timestamp, msg.value);
   }
 
@@ -53,7 +82,7 @@ contract OneShotSchedule {
 
     // Now failing transactions are forwarded. Is responsability of the requestor
     // to list a valid transaction
-    (bool success, bytes memory result) = metatransaction.to.call{ gas: metatransaction.gas, value: metatransaction.value }(metatransaction.data);
+    (bool success, bytes memory result) = metatransaction.to.call.gas(metatransaction.gas).value( metatransaction.value)(metatransaction.data);
 
     // The difference when calling gasleft() again is (aprox.) the gas used
     // in the call
