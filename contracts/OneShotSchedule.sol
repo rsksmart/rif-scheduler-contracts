92a51f4a72a80f5ee7a7a0919cb9851933c956ce
Diego Grasso <diegograsso@iovlab.org>
2021-04-16 17:31:09 -0300

    Added SafeMath

--- contracts/OneShotSchedule.sol
+++ contracts/OneShotSchedule.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 import "@rsksmart/erc677/contracts/IERC677.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 // import "@rsksmart/erc677/contracts/IERC677Receiver.sol";
 
 contract OneShotSchedule {
+  using SafeMath for uint;
+  using SafeMath for uint64;
+
   uint window;
   IERC677 token; 
   address providerAccount;
@@ -16,7 +20,7 @@ contract OneShotSchedule {
     address to;
     bytes data;
     uint gas;
-    uint timestamp;
+    uint64 timestamp;
     uint value;
     bool executed;
   }
@@ -24,7 +28,7 @@ contract OneShotSchedule {
   Metatransaction[] private transactionsScheduled;
 
   event SchedulingsPurchased(address indexed from, uint amount);
-  event MetatransactionAdded(uint indexed index, address indexed from, address indexed to, bytes data, uint gas, uint timestamp, uint value);
+  event MetatransactionAdded(uint indexed index, address indexed from, address indexed to, bytes data, uint gas, uint64 timestamp, uint value);
   event MetatransactionExecuted(uint indexed index, bool succes, bytes result);
 
   constructor(IERC677 _rifToken, address _providerAccount, uint _price, uint _window) public {
@@ -36,13 +40,13 @@ contract OneShotSchedule {
   }
 
   function _totalPrice(uint _amount) private view returns (uint){
-    return _amount * price;
+    return _amount.mul(price);
   }
 
   function purchase(uint _amount) public {
     require(token.allowance(msg.sender, address(this)) >= _totalPrice(_amount), 'Allowance Excedeed');
     token.transferFrom(msg.sender, address(this), _totalPrice(_amount));
-    remainingSchedulings[msg.sender] += _amount;
+    remainingSchedulings[msg.sender] = remainingSchedulings[msg.sender].add(_amount);
     emit SchedulingsPurchased(msg.sender, _amount);
   }
 
@@ -50,7 +54,7 @@ contract OneShotSchedule {
     require(address(token) == address(msg.sender),"Bad token");
     uint _schedulingAmount = abi.decode(_data, ( uint));
     require(_amount == _totalPrice(_schedulingAmount), "Transferred amount doens't match total purchase");
-    remainingSchedulings[_from] += _schedulingAmount ;
+    remainingSchedulings[_from] = remainingSchedulings[_from].add(_schedulingAmount);
     emit SchedulingsPurchased(_from, _schedulingAmount);
     return true;
   }
@@ -61,21 +65,21 @@ contract OneShotSchedule {
 
   function _spend(address _requestor) private {
     require(remainingSchedulings[_requestor] > 0, 'No balance available');
-    remainingSchedulings[_requestor] -= 1;
+    remainingSchedulings[_requestor] = remainingSchedulings[_requestor].sub(1);
   }
 
   function _refund(address _requestor) private {
-    remainingSchedulings[_requestor] += 1;
+    remainingSchedulings[_requestor] = remainingSchedulings[_requestor].add(1);
   }
 
-  function schedule(address to, bytes memory data, uint gas, uint executionTime) public payable {
+  function schedule(address to, bytes memory data, uint gas, uint64 executionTime) public payable {
     require(block.timestamp <= executionTime, 'Cannot schedule in past');
     _spend(msg.sender);
     transactionsScheduled.push(Metatransaction(msg.sender,to, data, gas, executionTime, msg.value, false));
     emit MetatransactionAdded(transactionsScheduled.length - 1, msg.sender, to, data, gas, executionTime, msg.value);
   }
 
-  function getSchedule(uint index) public view returns(address, address, bytes memory, uint, uint, uint, bool) {
+  function getSchedule(uint index) public view returns(address, address, bytes memory, uint, uint64, uint, bool) {
     Metatransaction memory metatransaction = transactionsScheduled[index];
     return (metatransaction.from, metatransaction.to, metatransaction.data, metatransaction.gas, metatransaction.timestamp, metatransaction.value, metatransaction.executed);
   }
@@ -88,8 +92,8 @@ contract OneShotSchedule {
     // Instead of just reverting, here we should:
     // - give the requestor a refund
     // - penalize the service provider
-    require(metatransaction.timestamp - window < block.timestamp, "Too soon");
-    require(metatransaction.timestamp + window >  block.timestamp, "Too late");
+    require(metatransaction.timestamp.sub(window) < block.timestamp, "Too soon");
+    require(metatransaction.timestamp.add(window) >  block.timestamp, "Too late");
 
     // We can use gasleft() here to charge the consumer for the gas
     // A contract may hold user's gas and charge it after executing
