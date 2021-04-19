6be189cda9e9d7e86cb0e31fbefc5f9dca8b74b3
Ilan Olkies <ilanolkies@outlook.com>
2021-04-19 18:56:32 -0300

    Fix lint

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Counter {
-  uint public count = 0;
+  uint256 public count = 0;
 
   function inc() public payable {
     count += count + 1;
   }
 
   function fail() public pure {
-    revert("Boom");
+    revert('Boom');
   }
 }
--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -1,6 +1,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@rsksmart/erc677/contracts/ERC677.sol";
+import '@rsksmart/erc677/contracts/ERC677.sol';
+
 // import "@rsksmart/erc677/contracts//ERC677TransferReceiver.sol";
 
-contract Imports {}
+contract Imports {
+
+}
