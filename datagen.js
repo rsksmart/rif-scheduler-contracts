866655e83f2f08f37f4b9f294138c76983564bb6
Ilan Olkies <ilanolkies@outlook.com>
2021-06-14 15:29:57 -0300

    Fix exported data

--- datagen.js
+++ datagen.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const OneShotSchedule = require('./build/contracts/OneShotSchedule');
 
-fs.writeFileSync('./OneShotScheduleABI.json', JSON.stringify({ abi: OneShotSchedule.abi }));
-fs.writeFileSync('./OneShotScheduleAddresses.json', JSON.stringify({ bytecode: OneShotSchedule.bytecode }));
-fs.writeFileSync('./OneShotScheduleBytecode.json', JSON.stringify({
+fs.writeFileSync('./OneShotSchedule.json', JSON.stringify({
+  abi: OneShotSchedule.abi,
+  bytecode: OneShotSchedule.bytecode
+}));
+
+fs.writeFileSync('./OneShotScheduleAddresses.json', JSON.stringify({
   address: {
     31: '0x0372f6f8c7b2353b546f842da0c44749664d1203'
   }
