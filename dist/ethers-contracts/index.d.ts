15b5e9e72e9b1f4a05114ce5ede84a8ffacbd909
Diego Grasso <diegograsso@iovlab.org>
2021-06-22 15:04:04 -0300

    Removed dist

--- dist/ethers-contracts/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type { RIFScheduler } from "./RIFScheduler";
-export { RIFScheduler__factory } from "./factories/RIFScheduler__factory";
-//# sourceMappingURL=index.d.ts.map
--- dist/ethers-contracts/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RIFScheduler__factory = void 0;
-var RIFScheduler__factory_1 = require("./factories/RIFScheduler__factory");
-Object.defineProperty(exports, "RIFScheduler__factory", { enumerable: true, get: function () { return RIFScheduler__factory_1.RIFScheduler__factory; } });
-//# sourceMappingURL=index.js.map
